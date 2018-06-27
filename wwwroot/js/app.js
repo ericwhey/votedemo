8806f2714836fd70ef338ca9bc44df71232d0359
Eric Hey <eric@erichey.com>
2018-06-27 02:33:15 -0700

    Added a bower package for qrcode.js and removed some unnecessary files.

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -17,6 +17,16 @@ angular.module("main",['angular-websocket']).controller('MyCtrl', function($scop
 
     var color = Chart.helpers.color;
 
+    window.chartColors = {
+        red: 'rgb(255, 99, 132)',
+        orange: 'rgb(255, 159, 64)',
+        yellow: 'rgb(255, 205, 86)',
+        green: 'rgb(75, 192, 192)',
+        blue: 'rgb(54, 162, 235)',
+        purple: 'rgb(153, 102, 255)',
+        grey: 'rgb(201, 203, 207)'
+    };
+
     var barChartData = {
         labels: [],
         datasets: [{
